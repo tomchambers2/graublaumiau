6d75f82b8ffe9ce6660764e4fd0841f7e19089f2
Tom Chambers <tom.chambers@gmail.com>
2017-03-02 15:14:33 +0000

    add links to imprint

--- src/components/Imprint.js
+++ src/components/Imprint.js
@@ -2,6 +2,9 @@ import React, { Component, PropTypes } from 'react';
 import {
   Image,
   StyleSheet,
+  Dimensions,
+  Linking,
+  View,
 } from 'react-native'
 
 import NavigationButton from './NavigationButton'
@@ -11,9 +14,17 @@ import backButton from '../assets/imprint/back.png'
 
 import globalStyles from '../globalStyles'
 
+const window = Dimensions.get('window')
+
 class Imprint extends Component {
   static propTypes = {
     navigator: PropTypes.object.isRequired,
+  }
+
+  goToWebsite = (url) => {
+    return () => {
+      Linking.openURL(url).catch(err => console.error('An error occurred', err));
+    }
   }
 
   handleGoBack = () => {
@@ -27,6 +38,20 @@ class Imprint extends Component {
             source={background}
             style={globalStyles.fullscreen}
         >
+            <NavigationButton
+                onPress={this.goToWebsite('http://alicekolb.ch/portfolio/')}
+                style={styles.link}
+              >
+              <View></View>
+            </NavigationButton>
+
+            <NavigationButton
+                onPress={this.goToWebsite('http://graublaumiau.ch/')}
+                style={styles.link2}
+              >
+              <View></View>
+            </NavigationButton>
+
             <NavigationButton
                 onPress={this.handleGoBack}
                 style={styles.backButton}
@@ -43,6 +68,26 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 30,
     top: 20,
+  },
+  link: {
+    position: 'absolute',
+    zIndex: 100,
+    width: 180,
+    height: 60,
+    top: 0.29 * window.width,
+    left: 0.08 * window.height,
+    // backgroundColor: 'yellow',
+    // opacity: 0.3,
+  },
+  link2: {
+    position: 'absolute',
+    zIndex: 100,
+    width: 220,
+    height: 60,
+    top: 0.36 * window.width,
+    left: 0.08 * window.height,
+    // backgroundColor: 'yellow',
+    // opacity: 0.3,
   },
 })
 
